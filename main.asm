8f59cd4a09606ee05a4f9c7a71d0a41bca621c97
MarcoUnie <mmagecoyt@gmail.com>
2026-03-04 14:54:02 +0000

    dia 3

--- main.asm
+++ main.asm
@@ -4,9 +4,24 @@
 .data
     msg db "Juego del Ahorcado$", 13, 10
     newline db 13,10,"$"
+    input db ?
     
 
 .code
+    print_newline proc
+    mov dx, offset newline
+    mov ah, 09h
+    int 21h
+    ret
+    print_newline endp
+    
+    read_letter proc
+    mov ah, 01h
+    int 21h
+    mov input, al
+    ret
+    read_letter endp
+
 main proc
     mov ax, @data
     mov ds, ax
@@ -18,11 +33,5 @@ main proc
     mov ah, 4Ch
     int 21h
 
-    print_newline proc
-    mov dx, offset newline
-    mov ah, 09h
-    int 21h
-    ret
-print_newline endp
 main endp
 end main
